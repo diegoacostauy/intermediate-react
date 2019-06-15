2f94b5b79fb0c37087e713f21ac0a23346366a44
diegaco <diegoacosta211@gmail.com>
2019-06-15 14:38:23 -0300

    emotion design systems & compound & animations

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,41 @@
 import React, { useState } from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
+import colors from "./colors";
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const Navbar = () => {
-  const [padding, setPadding] = useState(10);
+  const [padding] = useState(10);
+  let [speed, setSpeed] = useState(2);
+
   return (
     <header
       role="banner"
-      onClick={() => setPadding(padding + 1)}
       css={css`
-        background-color: #333;
+        background-color: ${colors.primary};
         padding: ${padding}px;
+        color: #fff;
       `}
     >
       <Link to="/">Adpot Me!</Link>
       <span
         role="img"
         aria-label="logo"
+        onClick={() => setSpeed(speed - 0.1 > 0 ? speed - 0.1 : speed)}
         css={css`
           font-size: 60px;
+          display: inline-block;
+          animation: ${speed}s ${spin} linear infinite;
+
+          &:hover {
+            text-decoration: underline;
+            animation-direction: reverse;
+          }
         `}
       >
         🐕
